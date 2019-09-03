67748f1aaaf7fd1e610d1b509254ddd64fd68bed
Tyler Koepke <t.koepke10190@gmail.com>
2019-09-03 15:57:35 -0500

    Added a customer model with properties of first name, last name, email, address, pick up day, and id. Also created a customers table to the database, and a customers view.

---
+++ TrashCollector/TrashCollector/TrashCollector/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ZipCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public double Balance { get; set; }
+        public int PickUpDayId { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+}
